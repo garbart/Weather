335f00c5656b345c294f92e0f89c511951c093c5
garbart <garbart@yandex.ru>
2022-11-29 12:57:53 +0300

    Initial commit

--- Weather/Controllers/ViewController.swift
+++ Weather/Controllers/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var feelsLikeTempLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func searchButtonPressed(_ sender: Any) {
+    }
+    
 }
 
---
+++ Weather/Supporting Files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Artem Garbart on 27.11.2022.
+//
+
+import Foundation
+
+
+let apiKey = "7ef93c2402498b00060f8b60efdd69c9"
